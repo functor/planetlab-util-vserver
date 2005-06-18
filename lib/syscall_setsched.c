d5175f76efea87a7211b0e39afbd21f2798616f2
Marc Fiuczynski <mef@cs.princeton.edu>
2005-06-18 06:08:22 +0000

    back ported from recent util-vserver

---
+++ lib/syscall_setsched.c
@@ -0,0 +1,42 @@
+// $Id: syscall_setsched.c,v 1.2 2004/09/22 20:45:37 ensc Exp $    --*- c -*--
+
+// Copyright (C) 2004 Enrico Scholz <enrico.scholz@informatik.tu-chemnitz.de>
+//  
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+//  
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//  
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+#include "compat.h"
+
+#include "vserver.h"
+
+#define VC_MULTIVERSION_SYSCALL	1
+#include "vserver-internal.h"
+
+#include "linuxvirtual.h"
+
+#define VC_ENABLE_API_V13
+
+#ifdef VC_ENABLE_API_V13
+#  include "syscall_setsched-v13.hc"
+#endif
+
+int
+vc_set_sched(xid_t xid, struct vc_set_sched const *data)
+{
+  CALL_VC(CALL_VC_V13B  (vc_set_sched,xid,data),
+	  CALL_VC_V13OBS(vc_set_sched,xid,data));
+}
