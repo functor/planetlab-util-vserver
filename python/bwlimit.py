85400c1d73e991d6214d36efadef1f3aedc7a54c
Mark Huang <mlhuang@cs.princeton.edu>
2007-02-07 04:25:37 +0000

    merge changes from head

--- python/bwlimit.py
+++ python/bwlimit.py
@@ -46,7 +46,7 @@
 # Mark Huang <mlhuang@cs.princeton.edu>
 # Copyright (C) 2006 The Trustees of Princeton University
 #
-# $Id: bwlimit.py,v 1.13 2006/11/27 22:32:59 mlhuang Exp $
+# $Id: bwlimit.py,v 1.15 2007/02/07 04:21:11 mlhuang Exp $
 #
 
 import sys, os, re, getopt
@@ -516,8 +516,12 @@ def on(xid, dev = dev, share = None, minrate = None, maxrate = None, minexemptra
         minrate = bwmin
     if minrate > maxrate:
         minrate = maxrate
+    if maxexemptrate < bwmin:
+        maxexemptrate = bwmin
     if maxexemptrate > bwmax:
         maxexemptrate = bwmax
+    if minexemptrate < bwmin:
+        minexemptrate = bwmin
     if minexemptrate > maxexemptrate:
         minexemptrate = maxexemptrate
 
